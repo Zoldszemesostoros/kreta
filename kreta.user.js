a624558f887aa44436700e85ceb6915baa54c3c3
Zoldszemesostoros <42379455+Zoldszemesostoros@users.noreply.github.com>
2019-05-29 20:25:27 +0200

    Minden weboldal bennnfoglalása

--- kreta.user.js
+++ kreta.user.js
@@ -1,9 +1,8 @@
-//az iskola kreta naplojanak cimet keretik bemasolni a @include sorba
 // ==UserScript==
 // @name     Kreta bejelentkezo
 // @version  1
 // @grant    none
-// @include  https://klik035242001.e-kreta.hu/Adminisztracio/Login
+// @include  https://*.e-kreta.hu/Adminisztracio/Login
 // ==/UserScript==
 var jelszo = localStorage.jelszo;
 var felhnev = localStorage.felhnev;
